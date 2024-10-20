01b9c0a6f73be4ca9efc27d21f51ed16132d5617
HaoYuanZheng0045 <haoyuan1@seas.upenn.edu>
2024-10-20 11:53:26 -0500

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -15,23 +15,20 @@ def pin_to_ipfs(data):
     # Convert the data to JSON and prepare it for upload
     json_data = json.dumps(data)
     files = {
-        'file': ('data.json', json_data, 'application/json')  # 添加MIME类型
+        'file': ('data.json', json_data)
     }
     
-    try:
-        # Send the POST request to the IPFS API to add the file
-        response = requests.post(url, files=files)
-        response.raise_for_status()  # 如果响应状态码不是 200，抛出异常
-        
+    # Send the POST request to the IPFS API to add the file
+    response = requests.post(url, files=files)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
         response_json = response.json()
-        cid = response_json.get('Hash')
-        
-        if not cid:
-            raise Exception("Failed to obtain CID from IPFS response.")
-        
-        return str(cid)  # Ensure CID is returned as a string
-    except requests.exceptions.RequestException as e:
-        raise Exception("Failed to pin data to IPFS: {}".format(e))
+        cid = response_json['Hash']
+    else:
+        raise Exception("Failed to pin data to IPFS: {}".format(response.text))
+    
+    return str(cid)  # Ensure CID is returned as a string
 
 def get_from_ipfs(cid, content_type="json"):
     """
@@ -40,37 +37,86 @@ def get_from_ipfs(cid, content_type="json"):
     """
     assert isinstance(cid, str), "get_from_ipfs expects a CID in the form of a string"
     
-    # Try using different gateways
-    gateways = [
-        f"https://cloudflare-ipfs.com/ipfs/{cid}",
-        f"https://ipfs.io/ipfs/{cid}",
-        f"https://gateway.pinata.cloud/ipfs/{cid}",
-        f"https://gateway.moralisipfs.com/ipfs/{cid}"
-    ]
+    # URL for retrieving the data from the IPFS gateway
+    gateway_url = "http://localhost:8080/ipfs/{}".format(cid)
     
-    for gateway_url in gateways:
-        try:
-            response = requests.get(gateway_url)
-            response.raise_for_status()  # If status code is not 200, raise an exception
-            
-            if content_type == "json":
-                try:
-                    data = response.json()
-                    assert isinstance(data, dict), "get_from_ipfs should return a dictionary when content_type is 'json'"
-                except json.JSONDecodeError:
-                    raise Exception("The content retrieved is not valid JSON.")
-            else:
-                data = response.content
-            
-            return data
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to retrieve data from {gateway_url}: {e}")
+    # Send GET request to fetch the file from IPFS
+    response = requests.get(gateway_url)
     
-    raise Exception("Failed to retrieve data from any IPFS gateway")
+    # Check if the request was successful
+    if response.status_code == 200:
+        if content_type == "json":
+            data = response.json()
+        else:
+            data = response.content
+    else:
+        raise Exception("Failed to retrieve data from IPFS: {}".format(response.text))
+    
+    if content_type == "json":
+        assert isinstance(data, dict), "get_from_ipfs should return a dictionary"
+    
+    return data
 
 if __name__ == "__main__":
-    # 使用你想要测试的 CID
-    cid = "QmZKxhYycNWK4wkqkpt2gbaqzzZUZ5op5kmNeFZo"
+    # Data to be pinned to IPFS
+    data_to_pin = {
+        "title": "EVERYDAYS: THE FIRST 5000 DAYS",
+        "name": "EVERYDAYS: THE FIRST 5000 DAYS",
+        "type": "object",
+        "imageUrl": "https://ipfsgateway.makersplace.com/ipfs/QmZ15eQX8FPjfrtdX3QYbrhZxJpbLpvDpsgb2p3VEH8Bqq",
+        "description": "I made a picture from start to finish every single day from May 1st, 2007 - January 7th, 2021. This is every one of those pictures.",
+        "attributes": [
+            {
+                "trait_type": "Creator",
+                "value": "beeple"
+            }
+        ],
+        "properties": {
+            "name": {
+                "type": "string",
+                "description": "EVERYDAYS: THE FIRST 5000 DAYS"
+            },
+            "description": {
+                "type": "string",
+                "description": "I made a picture from start to finish every single day from May 1st, 2007 - January 7th, 2021. This is every one of those pictures."
+            },
+            "preview_media_file": {
+                "type": "string",
+                "description": "https://ipfsgateway.makersplace.com/ipfs/QmZ15eQX8FPjfrtdX3QYbrhZxJpbLpvDpsgb2p3VEH8Bqq"
+            },
+            "preview_media_file_type": {
+                "type": "string",
+                "description": "jpg"
+            },
+            "created_at": {
+                "type": "datetime",
+                "description": "2021-02-16T00:07:31.674688+00:00"
+            },
+            "total_supply": {
+                "type": "int",
+                "description": 1
+            },
+            "digital_media_signature_type": {
+                "type": "string",
+                "description": "SHA-256"
+            },
+            "digital_media_signature": {
+                "type": "string",
+                "description": "6314b55cc6ff34f67a18e1ccc977234b803f7a5497b94f1f994ac9d1b896a017"
+            },
+            "raw_media_file": {
+                "type": "string",
+                "description": "https://ipfsgateway.makersplace.com/ipfs/QmXkxpwAHCtDXbbZHUwqtFucG1RMS6T87vi1CdvadfL7qA"
+            }
+        }
+    }
+    
+    try:
+        # Pin data to IPFS
+        cid = pin_to_ipfs(data_to_pin)
+        print("Data successfully pinned to IPFS with CID: {}".format(cid))
+    except Exception as e:
+        print(e)
 
     # Retrieve the data using the CID
     if isinstance(cid, str):
