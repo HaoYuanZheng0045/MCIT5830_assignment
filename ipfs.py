02923dad5868daeb53b4f7ffe0618dd6fabd42f8
HaoYuanZheng0045 <haoyuan1@seas.upenn.edu>
2024-10-20 11:39:56 -0500

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -44,7 +44,8 @@ def get_from_ipfs(cid, content_type="json"):
     gateways = [
         f"https://cloudflare-ipfs.com/ipfs/{cid}",
         f"https://ipfs.io/ipfs/{cid}",
-        f"https://gateway.pinata.cloud/ipfs/{cid}"
+        f"https://gateway.pinata.cloud/ipfs/{cid}",
+        f"https://gateway.moralisipfs.com/ipfs/{cid}"
     ]
     
     for gateway_url in gateways:
@@ -68,65 +69,8 @@ def get_from_ipfs(cid, content_type="json"):
     raise Exception("Failed to retrieve data from any IPFS gateway")
 
 if __name__ == "__main__":
-    # Data to be pinned to IPFS
-    data_to_pin = {
-        "title": "EVERYDAYS: THE FIRST 5000 DAYS",
-        "name": "EVERYDAYS: THE FIRST 5000 DAYS",
-        "type": "object",
-        "imageUrl": "https://ipfsgateway.makersplace.com/ipfs/QmZ15eQX8FPjfrtdX3QYbrhZxJpbLpvDpsgb2p3VEH8Bqq",
-        "description": "I made a picture from start to finish every single day from May 1st, 2007 - January 7th, 2021. This is every one of those pictures.",
-        "attributes": [
-            {
-                "trait_type": "Creator",
-                "value": "beeple"
-            }
-        ],
-        "properties": {
-            "name": {
-                "type": "string",
-                "description": "EVERYDAYS: THE FIRST 5000 DAYS"
-            },
-            "description": {
-                "type": "string",
-                "description": "I made a picture from start to finish every single day from May 1st, 2007 - January 7th, 2021. This is every one of those pictures."
-            },
-            "preview_media_file": {
-                "type": "string",
-                "description": "https://ipfsgateway.makersplace.com/ipfs/QmZ15eQX8FPjfrtdX3QYbrhZxJpbLpvDpsgb2p3VEH8Bqq"
-            },
-            "preview_media_file_type": {
-                "type": "string",
-                "description": "jpg"
-            },
-            "created_at": {
-                "type": "datetime",
-                "description": "2021-02-16T00:07:31.674688+00:00"
-            },
-            "total_supply": {
-                "type": "int",
-                "description": 1
-            },
-            "digital_media_signature_type": {
-                "type": "string",
-                "description": "SHA-256"
-            },
-            "digital_media_signature": {
-                "type": "string",
-                "description": "6314b55cc6ff34f67a18e1ccc977234b803f7a5497b94f1f994ac9d1b896a017"
-            },
-            "raw_media_file": {
-                "type": "string",
-                "description": "https://ipfsgateway.makersplace.com/ipfs/QmXkxpwAHCtDXbbZHUwqtFucG1RMS6T87vi1CdvadfL7qA"
-            }
-        }
-    }
-    
-    try:
-        # Pin data to IPFS
-        cid = pin_to_ipfs(data_to_pin)
-        print("Data successfully pinned to IPFS with CID: {}".format(cid))
-    except Exception as e:
-        print(e)
+    # 使用你想要测试的 CID
+    cid = "QmZKxhYycNWK4wkqkpt2gbaqzzZUZ5op5kmNeFZo"
 
     # Retrieve the data using the CID
     if isinstance(cid, str):
@@ -137,3 +81,4 @@ if __name__ == "__main__":
             print(e)
     else:
         print("Error: The CID is not in a string format.")
+
