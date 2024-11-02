46c66f52907335468026092059eca2d38f69137a
HaoYuanZheng0045 <haoyuan1@seas.upenn.edu>
2024-11-02 14:11:19 -0500

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -2,7 +2,7 @@ from web3 import Web3
 import eth_account
 import os
 
-def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
+def get_keys(challenge, keyId=0, filename="eth_mnemonic.txt"):
     """
     Generate a stable private key
     challenge - byte string
@@ -17,16 +17,17 @@ def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
 
     msg = eth_account.messages.encode_defunct(challenge)
 
-	#YOUR CODE HERE
+    # 读取或生成助记词
     if os.path.exists(filename):
         with open(filename, "r") as f:
             mnemonics = f.readlines()
     else:
         mnemonics = []
 
+    # 生成新的私钥（如果没有足够的助记词）
     if keyId >= len(mnemonics):
         acct = eth_account.Account.create()
-        private_key = acct.privateKey.hex()
+        private_key = acct._private_key.hex()  # 使用 _private_key 而不是 privateKey
         mnemonics.append(private_key + "\n")
         with open(filename, "w") as f:
             f.writelines(mnemonics)
@@ -36,16 +37,17 @@ def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
     acct = eth_account.Account.from_key(private_key)
     eth_addr = acct.address
 
+    # 对消息进行签名
     sig = acct.sign_message(msg)
 
+    # 验证签名
+    assert eth_account.Account.recover_message(msg, signature=sig.signature.hex()) == eth_addr, "Failed to sign message properly"
 
-    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
-
-    #return sig, acct #acct contains the private key
+    # 返回签名和地址
     return sig, eth_addr
 
 if __name__ == "__main__":
     for i in range(4):
         challenge = os.urandom(64)
-        sig, addr= get_keys(challenge=challenge,keyId=i)
-        print( addr )
+        sig, addr = get_keys(challenge=challenge, keyId=i)
+        print(addr)
