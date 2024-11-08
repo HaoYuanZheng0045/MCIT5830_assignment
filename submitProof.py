e1c55be056f778146bbad69cde19d817ab79332b
HaoYuanZheng0045 <haoyuan1@seas.upenn.edu>
2024-11-07 18:08:19 -0600

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -72,21 +72,31 @@ def sign_challenge(challenge):
 
 
 def send_signed_msg(proof, random_leaf):
+    """
+        Takes a Merkle proof of a leaf, and that leaf (in bytes32 format)
+        builds signs and sends a transaction claiming that leaf (prime)
+        on the contract
+    """
     chain = 'bsc'
     acct = get_account()
     address, abi = get_contract_info(chain)
     w3 = connect_to(chain)
     contract = w3.eth.contract(address=address, abi=abi)
     
-    # 使用手动设置的 gasPrice
-    tx = contract.functions.submit(proof, random_leaf).transact({
+    # 使用 buildTransaction 而不是 transact
+    tx = contract.functions.submit(proof, random_leaf).buildTransaction({
         'from': acct.address,
+        'nonce': w3.eth.getTransactionCount(acct.address),
         'gas': 300000,
-        'gasPrice': 20 * (10 ** 9)  # 20 Gwei 转换为 Wei
+        'gasPrice': 20 * (10 ** 9)  # 手动设置为20 Gwei
     })
     
-    tx_hash = w3.toHex(tx)
-    return tx_hash
+    # 对交易进行签名
+    signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
+    
+    # 使用 send_raw_transaction 发送签名后的交易
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    return tx_hash.hex()
 
 
 # Helper functions that do not need to be modified
